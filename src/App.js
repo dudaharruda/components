1bae409bc4d929cf6d9dc3851784abb74f39d47d
dudah arruda <arrudamariaeduarda492@gmail.com>
2023-09-01 14:39:17 -0300

    login com material ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import"./app.css"
+import Formulario from "./components/Formulario/formulario";
+
 function App() {
   return (
-    <></>
+    <>
+    <Formulario/>
+    </>
   );
 }
-
 export default App;
+
